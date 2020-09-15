76a406427a2b972eded10377f9042978b8cb98a3
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2020-09-15 22:18:01 +0300

    feat: Introduce solution template
    To speed up development.
    
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

---
+++ solution_template.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#ifdef DEBUG
+#include <gtest/gtest.h>
+
+#include "utils/timer.h"
+#endif
+
+namespace {
+unsigned solution(long long n) {
+    return n;
+}
+
+#ifdef DEBUG
+void test_solution() {
+    auto timer = Timer();
+    EXPECT_EQ(solution(1), 1);
+    ASSERT_LE(timer.elapsed(), 1000);
+
+    timer.restart();
+    EXPECT_EQ(solution(2), 2);
+    ASSERT_LE(timer.elapsed(), 1000);
+}
+#endif
+} // namespace
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        long long n;
+        std::cin >> n;
+
+        std::cout << solution(n) << std::endl;
+        return 0;
+    }
+
+#ifdef DEBUG
+    if (std::string(argv[1]) == "test") {
+        test_solution();
+        return 0;
+    }
+#endif
+
+    return 1;
+}
